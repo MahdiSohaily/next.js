163f254abcec0c1e0bdff8471de913017a8f2735
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-08 21:15:28 +0430

    update link address

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+        Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
