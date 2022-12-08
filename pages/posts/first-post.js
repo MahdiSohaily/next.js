33850da9a1e672e4cf5c06a1a7d9c26abce4bd48
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-08 22:03:53 +0430

    Add profile image to the [age

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
