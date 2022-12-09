75847e3c1084a999ffc7065f1f4ff8931d8199ab
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-09 09:13:17 +0430

    Update style

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
 import styles from './layout.module.css';
 export default function Layout({ children }) {
-  return <div className="container">{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 }
