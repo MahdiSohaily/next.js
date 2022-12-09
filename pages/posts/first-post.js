83cc1ecc55e0edb36ceb59814ee02f612eacc6ef
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-09 18:50:14 +0430

    Update utilities page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,21 +3,5 @@ import Image from 'next/image';
 import Head from 'next/head';
 import Layout from '../../components/layout';
 export default function FirstPost() {
-  return (
-    <Layout>
-      <Head>
-        <title>My first post</title>
-      </Head>
-      <h1>First Post</h1>
-      <Image
-        src="/images/profile.jpg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-      />
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </Layout>
-  );
+  return <Layout></Layout>;
 }
