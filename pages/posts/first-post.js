342bf59246e343cd011d6daffb106ad45bedae13
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-09 08:47:14 +0430

    Add page title

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>My first post</title>
+      </Head>
+      
       <h1>First Post</h1>
       <Image
         src="/images/profile.jpg" // Route of the image file
