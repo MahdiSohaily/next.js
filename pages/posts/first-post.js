df3057262868a376659c75596deb398bd0b8ad3d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-09 10:50:31 +0430

    Remove unwanted script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
-import Script from 'next/script';
 import Layout from '../../components/layout';
 export default function FirstPost() {
   return (
@@ -9,13 +8,6 @@ export default function FirstPost() {
       <Head>
         <title>My first post</title>
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
       <h1>First Post</h1>
       <Image
         src="/images/profile.jpg" // Route of the image file
