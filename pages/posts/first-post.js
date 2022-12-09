bccaf340f006ec19744ba8a27a8d96d4952c3183
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-09 08:47:36 +0430

    Add facebook sdk

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,20 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
+import Script from 'next/script';
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>My first post</title>
       </Head>
-      
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <Image
         src="/images/profile.jpg" // Route of the image file
