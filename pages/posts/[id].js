4c9ca2e84043dbf7177b595f9c03a1655f9624f1
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-11 09:58:51 +0430

    Add static props

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -7,3 +7,7 @@ export default function Post() {
 export async function getStaticPaths() {
   // Return a list of possible value for id
 }
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+}
