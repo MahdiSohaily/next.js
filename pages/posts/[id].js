8efbafb866bd43ae906d75b8a0f24c2d7662bdde
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-11 09:58:10 +0430

    Add static paths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,3 +3,7 @@ import Layout from '../../components/layout';
 export default function Post() {
   return <Layout>...</Layout>;
 }
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+}
