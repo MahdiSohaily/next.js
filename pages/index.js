8fdc4b6aaf6a09abc5a985af62580bbda1cabbeb
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-11 09:55:46 +0430

    Add dynamic route for posts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
-import { getSortedPostsData } from '../lib/posts';
+import { getSortedPostsData } from '../libs/posts';
 import utilStyles from '../styles/utils.module.css';
 
 export default function Home({ allPostsData }) {
