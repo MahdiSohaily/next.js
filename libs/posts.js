3ac21c871b3ed543a3c1f1187cc04b76b2a9b003
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-11 10:05:46 +0430

    Add function to fetch posts id

--- libs/posts.js
+++ libs/posts.js
@@ -33,3 +33,14 @@ export function getSortedPostsData() {
     }
   });
 }
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    };
+  });
+}
